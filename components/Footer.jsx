aeeccf0e603a5fdb43eadf61a9b6884767b241e2
Ansh <anshsaxena4190@gmail.com>
2023-05-07 18:06:07 +0530

    footer UI

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <div>
-            <p>footer</p>
+        <div className="hero-wrapper !h-auto  relative mt-4">
+            <div className="relative mt-8 flex justify-between overflow-hidden rounded-[36px] p-8 ">
+                <img src="./logo.png" alt="" width="200px" />
+
+                <p>Get your events authenticated!</p>
+
+                <Link href="https://github.com/anshss/Gatify">
+                    <div className="flex flex-row gap-2">
+                        <img src="./github.svg" alt="" />
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
