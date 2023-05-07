12ced1c38b7f61393ef3a253f2f49186938c632b
Ansh <anshsaxena4190@gmail.com>
2023-05-07 14:39:36 +0530

    navbar UI

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import LoginWrapper from "./LoginWrapper";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+    const router = useRouter();
+
+    const isActive = (pathname) => {
+        return router.pathname === pathname;
+    };
+
     return (
-        <div className="flex gap-3">
-            <Link href="/">Home</Link>
-            <Link href="/events">Events</Link>
-            <Link href="/host">Host</Link>
-            <Link href="/dashboard">Dashboard</Link>
+        <div className="flex justify-around p-6">
+            <img src="./logo.png" alt="" width="150px" />
+            <ul className="flex gap-3">
+                <li className={isActive('/') ? "text-purple-500" : ''}>
+                    <Link className="hover:opacity-75 lg:pl-5 lg:pr-5" href="/">Home</Link>
+                </li>
+                <li className={isActive('/events') ? "text-purple-500" : ''}>
+                    <Link className="hover:opacity-75 lg:pl-5 lg:pr-5" href="/events">Events</Link>
+                </li>
+                <li className={isActive('/host') ? "text-purple-500" : ''}>
+                    <Link className="hover:opacity-75 lg:pl-5 lg:pr-5" href="/host">Host</Link>
+                </li>
+                <li className={isActive('/dashboard') ? "text-purple-500" : ''}>
+                    <Link className="hover:opacity-75 lg:pl-5 lg:pr-5" href="/dashboard">Dashboard</Link>
+                </li>
+            </ul>
             <LoginWrapper />
         </div>
     );
---
+++ pages/test.js
@@ -0,0 +1,8 @@
+export default function Test() {
+    return (
+        <div>
+            <p>upload page</p>
+            
+        </div>
+    )
+}
